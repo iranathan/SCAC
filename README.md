cbbd6da716ba0e7a41efa6432954c23d526adb7b
ItsPugle <22669894+ItsPugle@users.noreply.github.com>
2018-05-28 20:02:29 +1000

    Publicising source code

---
+++ README.md
@@ -0,0 +1,41 @@
+# Spotify Content Availability Checker
+SCAC allows anyone to find the markets where Spotify songs can be streamed :headphones:  
+
+**Why?** Because knowing about music licensing can be hard to stay "in the loop" about; and that's what SCAC is for. When music is truely available to all, it takes its trueform of inspiration.
+
+## Get Started
+Using SCAC is as easy as one-two-three:
+1. Head to https://scac.itspugle.ga and click "Search",
+2. Authenticate the tool by pressing "OKAY" on the Spotify authorisation page,
+3. Paste in the Spotify URL or URI of a track and click "Search"!
+
+![Demonstration of the tool](/assets/demo.gif)
+
+## Contributing
+Contributing to SCAC fills the hearts of many, and helps to keep this project constantly moving. It's really easy to get started with contributing ideas and issues, so check out [the contributing guide](/.github/CONTRIBUTING) to get started.
+
+## License
+MIT License
+
+Copyright (c) 2018 ItsPugle
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+###### Disclosure
+SCAC is in no way supported, approved or otherwise representative of Spotify. This is just a solo project, designed to help the masses.
---
+++ javascript/main.js
@@ -0,0 +1,74 @@
+"use strict";
+
+function parse() {
+  var trackID = document.getElementById("trackInput").value; // Read value of trackInput for ease in processing.
+  var invalid = "<div class='alert alert-danger' role='alert'><h4 class='alert-heading'>Uh oh! Something's not right.</h4><hr><p>It looks like you might be entering something other than a Spotify, or not entering anything at all!<br>Make sure that you're entering a valid Spotify <abbr title='For example, https://open.spotify.com/track/123456789abcdefghijklm'>URL</abbr> or <abbr title='For example, spotify:track:123456789abcdefghijklm'>URI</abbr> of a track.</p></div>"; // Base error message for issues
+  var error = "<p><strong>Think you've found an issue?</strong> Let me know by clicking <code>Report issues or suggest features</code> in the footer.</p>" // Issue reporting prompt
+  
+  if (trackID.includes("https://open.spotify.com/track/")) { // trackInput == Spotify URL
+    trackID = trackID.slice(31, 53);
+  } else if (trackID.includes("spotify:track:")) { // trackInput == Spotify URI
+    trackID = trackID.slice(14, 36);
+  } else { // trackInput == not recognised
+    document.getElementById("warnings").style.display = "inline";
+    document.getElementById("warnings").innerHTML = invalid + error;
+    document.getElementById("playButton").innerHTML = "";
+    document.getElementById("description").innerHTML = "";
+    document.getElementById("countries").innerHTML = "";  
+  }
+  
+  search(trackID);
+}
+
+function search(trackID) {
+  // Get the hash of the url
+  var hash = window.location.hash.substring(1).split("&").reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+  // Define authentication endpoint and token
+  var token = hash.access_token;
+  var authEndpoint = "https://accounts.spotify.com/authorize";
+
+  // Proxide the prefix and suffix for embed/track preview
+  var embedPrefix = "<iframe src='https://open.spotify.com/embed?uri=spotify:track:";
+  var embedSuffix = "' width='400' height='80' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>";
+
+  // Replace with the Production clientId and redirectUri
+  var clientId = "c87ccc0ea8804a7b8b38d81ab3521827";
+  var redirectUri = "https://scac.itspugle.ga";
+  
+  if (!token) {
+    window.location = authEndpoint + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=token&show_dialog=true";
+  } else {
+    $.ajax({
+      url: "https://api.spotify.com/v1/tracks/" + trackID,
+      type: "GET",
+      beforeSend: function beforeSend(xhr) {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: function success(data) {
+        var str = data.available_markets;
+        var trackName = JSON.stringify(data.name);
+        if (str.toString() == "") {
+          trackName = trackName.slice(1, trackName.length - 1);
+          document.getElementById("description").innerHTML = "<strong>" + trackName + "</strong> song is available anywhere Spotify is available.";
+          document.getElementById("countries").outerHTML = "";
+          document.getElementById("playButton").innerHTML = embedPrefix + trackID + embedSuffix;
+          document.getElementById("authAlert").style.display = "none";
+        } else {
+          var markets = "<ul><li>" + str.join("</li><li>") + "</li></ul>";
+          trackName = trackName.slice(1, trackName.length - 1);
+          document.getElementById("description").innerHTML = "<strong>" + trackName + "</strong> is available in these countries:";
+          document.getElementById("countries").innerHTML = markets;
+          document.getElementById("playButton").innerHTML = embedPrefix + trackID + embedSuffix;
+          document.getElementById("authAlert").style.display = "none";
+        }
+      }
+    });
+  }
+}
