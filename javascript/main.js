8406d3fe2698c56b35125ca96e8f2e2b85de33fa
ItsPugle <git@itspugle.ga>
2018-06-02 21:57:49 +1000

    Adding the counties and a function

--- javascript/main.js
+++ javascript/main.js
@@ -18,7 +18,7 @@ function parse() {
   }
   
   search(trackID);
-}
+};
 
 function search(trackID) {
   // Get the hash of the url
@@ -61,6 +61,11 @@ function search(trackID) {
           document.getElementById("playButton").innerHTML = embedPrefix + trackID + embedSuffix;
           document.getElementById("authAlert").style.display = "none";
         } else {
+          str.forEach(function(){
+            var index = str.IndexOf();
+            var country = countries.code;
+            str[index] = country;
+          });
           var markets = "<ul><li>" + str.join("</li><li>") + "</li></ul>";
           trackName = trackName.slice(1, trackName.length - 1);
           document.getElementById("description").innerHTML = "<strong>" + trackName + "</strong> is available in these countries:";
@@ -71,4 +76,77 @@ function search(trackID) {
       }
     });
   }
-}
+};
+
+var countries = {
+  // Asia
+  AU: "Australia",
+  JP: "Japan",
+  IL: "Israel",
+  HK: "Hong Kong",
+  ID: "Indonesia",
+  MY: "Malaysia",
+  NZ: "New Zealand",
+  PH: "Philippines",
+  SG: "Singapore",
+  TW: "Taiwan",
+  TH: "Thailand",
+  VN: "Vietnam",
+  // Europe
+  AD: "Andorra",
+  AT: "Austria",
+  BE: "Belgium",
+  BG: "Bulgaria",
+  CY: "Cyprus",
+  CZ: "Czech Republic",
+  DK: "Denmark",
+  EE: "Estonia",
+  FI: "Finland",
+  FR: "France",
+  DE: "Germany",
+  GR: "Greece",
+  HU: "Hungary",
+  IS: "Iceland",
+  IE: "Ireland",
+  IT: "Italy",
+  LV: "Latvia",
+  LI: "Liechtenstien",
+  LT: "Lithuania",
+  LU: "Luxembourg",
+  MT: "Malta",
+  MC: "Monaco",
+  NL: "Netherlands",
+  NO: "Norway",
+  PL: "Poland",
+  PT: "Portugal",
+  RO: "Romania",
+  SK: "Slovakia",
+  ES: "Spain",
+  SE: "Sweden",
+  CH: "Switzerland",
+  TR: "Turkey",
+  UK: "United Kingdom",
+  // Latin America and the Caribbean
+  AR: "Argentina",
+  BO: "Bolivia",
+  BR: "Brazil",
+  CL: "Chile",
+  CO: "Colombia",
+  CR: "Costa Rica",
+  DO: "Dominican Republic",
+  EC: "Ecuador",
+  SV: "El Savador",
+  GT: "Guatamala",
+  HN: "Honduras",
+  MX: "Mexico",
+  NI: "Nicaragua",
+  PA: "Panama",
+  PY: "Paraguy",
+  PE: "Peru",
+  UY: "Uruguay",
+  // North America
+  CA: "Canada",
+  US: "United States",
+  // Africa
+  ZA: "South Africa"
+};
