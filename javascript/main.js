792345279b3320f523e1e371ba0928e11574428a
ItsPugle <git@itspugle.ga>
2018-06-03 23:08:26 +1000

    Solving most JSLint issues

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,8 @@
-"use strict";
-
 function parse() {
-  var trackID = document.getElementById("trackInput").value; // Read value of trackInput for ease in processing.
-  var invalid = "<div class='alert alert-danger' role='alert'><h4 class='alert-heading'>Uh oh! Something's not right.</h4><hr><p>It looks like you might be entering something other than a Spotify, or not entering anything at all!<br>Make sure that you're entering a valid Spotify <abbr title='For example, https://open.spotify.com/track/123456789abcdefghijklm'>URL</abbr> or <abbr title='For example, spotify:track:123456789abcdefghijklm'>URI</abbr> of a track.</p></div>"; // Base error message for issues
-  var error = "<p><strong>Think you've found an issue?</strong> Let me know by clicking <code>Report issues or suggest features</code> in the footer.</p>" // Issue reporting prompt
+  "use strict";
+  var trackID = document.getElementById("trackInput").value,
+    invalid = "<div class='alert alert-danger' role='alert'><h4 class='alert-heading'>Uh oh! Something's not right.</h4><hr><p>It looks like you might be entering something other than a Spotify, or not entering anything at all!<br>Make sure that you're entering a valid Spotify <abbr title='For example, https://open.spotify.com/track/123456789abcdefghijklm'>URL</abbr> or <abbr title='For example, spotify:track:123456789abcdefghijklm'>URI</abbr> of a track.</p></div>",
+    error = "<p><strong>Think you've found an issue?</strong> Let me know by clicking <code>Report issues or suggest features</code> in the footer.</p>";
   
   if (trackID.includes("https://open.spotify.com/track/")) { // trackInput == Spotify URL
     trackID = trackID.slice(31, 53);
@@ -12,35 +11,29 @@ function parse() {
   } else { // trackInput == not recognised
     document.getElementById("warnings").style.display = "inline";
     document.getElementById("warnings").innerHTML = invalid + error;
-    document.getElementById("playButton").innerHTML = "";
-    document.getElementById("description").innerHTML = "";
-    document.getElementById("countries").innerHTML = "";  
+    document.getElementById("playButton").innerHTML = null;
+    document.getElementById("description").innerHTML = null;
+    document.getElementById("countries").innerHTML = null;
   }
   
   search(trackID);
-};
+}
 
 function search(trackID) {
-  // Get the hash of the url
+  "use strict";
   var hash = window.location.hash.substring(1).split("&").reduce(function (initial, item) {
     if (item) {
       var parts = item.split("=");
       initial[parts[0]] = decodeURIComponent(parts[1]);
     }
     return initial;
-  }, {});
-
-  // Define authentication endpoint and token
-  var token = hash.access_token;
-  var authEndpoint = "https://accounts.spotify.com/authorize";
-
-  // Proxide the prefix and suffix for embed/track preview
-  var embedPrefix = "<iframe src='https://open.spotify.com/embed?uri=spotify:track:";
-  var embedSuffix = "' width='400' height='80' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>";
-
-  // Replace with the Production clientId and redirectUri
-  var clientId = "c87ccc0ea8804a7b8b38d81ab3521827";
-  var redirectUri = "https://scac.itspugle.ga";
+  }, {}),
+    token = hash.access_token,
+    authEndpoint = "https://accounts.spotify.com/authorize",
+    embedPrefix = "<iframe src='https://open.spotify.com/embed?uri=spotify:track:",
+    embedSuffix = "' width='400' height='80' frameborder='0' allowtransparency='true' allow='encrypted-media'></iframe>",
+    clientId = "c87ccc0ea8804a7b8b38d81ab3521827",
+    redirectUri = "https://scac.itspugle.ga";
   
   if (!token) {
     window.location = authEndpoint + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=token&show_dialog=true";
@@ -52,20 +45,15 @@ function search(trackID) {
         xhr.setRequestHeader("Authorization", "Bearer " + token);
       },
       success: function success(data) {
-        var str = data.available_markets;
-        var trackName = JSON.stringify(data.name);
-        if (str.toString() == "") {
+        var str = data.available_markets,
+          trackName = JSON.stringify(data.name);
+        if (str.toString() === "") {
           trackName = trackName.slice(1, trackName.length - 1);
           document.getElementById("description").innerHTML = "<strong>" + trackName + "</strong> song is available anywhere Spotify is available.";
           document.getElementById("countries").outerHTML = "";
           document.getElementById("playButton").innerHTML = embedPrefix + trackID + embedSuffix;
           document.getElementById("authAlert").style.display = "none";
         } else {
-          str.forEach(function(){
-            var index = str.IndexOf();
-            var country = countries.code;
-            str[index] = country;
-          });
           var markets = "<ul><li>" + str.join("</li><li>") + "</li></ul>";
           trackName = trackName.slice(1, trackName.length - 1);
           document.getElementById("description").innerHTML = "<strong>" + trackName + "</strong> is available in these countries:";
@@ -76,4 +64,4 @@ function search(trackID) {
       }
     });
   }
-};
+}
