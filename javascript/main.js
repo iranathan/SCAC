36600d6f7e65365ff9f84ad29bb96e9addf4de2c
Tim <git@itspugle.ga>
2018-06-28 13:41:06 +1000

    Hovering over disabled links shows disabled message

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,11 @@
+var disabledLinks = document.getElementsByClassName("link-disabled");
+for (var i = 0; i < disabledLinks.length; i++) {
+  var content = disabledLinks[i].innerHTML;
+  disabledLinks[i].setAttribute("onmouseover", "this.innerHTML = 'This link has been disabled'");
+  disabledLinks[i].setAttribute("onmouseout", "this.innerHTML = '" + content + "'");
+  disabledLinks[i].setAttribute("href", "#");
+}
+
 function parse() {
   "use strict";
   var trackID = document.getElementById("trackInput").value,
