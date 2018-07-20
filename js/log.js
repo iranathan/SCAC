6bc3adae30d988d7dab6ffa503345dc0c75be64b
Tim <tim@itspugle.ga>
2018-07-20 09:13:51 +0000

    Adding vulnerability testing

--- js/log.js
+++ js/log.js
@@ -1,4 +1,4 @@
 /* Console Logging */
-var version = "2.0.0.0";
+var version = "2.0.1.0";
 document.getElementById("version").innerHTML = "v" + version;
 console.log("You're loading SCAC with deploy version of " + version);
