a28cfbee7ec8b069a5bc5394a35ddd8327d8787d
ItsPugle <git@itspugle.ga>
2018-08-07 17:57:11 +1000

    Prevent attempting to change DOM with version if not at about page

--- js/log.js
+++ js/log.js
@@ -1,4 +1,9 @@
 /* Console Logging */
 var version = "2.0.1.0";
-document.getElementById("version").innerHTML = "v" + version;
+if (window.location.href === "https://scac.itspugle.ga/about.html") {
+  document.getElementById("version").innerHTML = "v" + version;
+
+} else {
+  console.log("Because you're not on about.html, we won't attempt to publish the version.")
+}
 console.log("You're loading SCAC with deploy version of " + version);
